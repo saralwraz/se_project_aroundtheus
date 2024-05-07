225fd7dfcb00002370ec139244b1f720479428b3
Sara Raz <saralwraz@gmail.com>
2024-05-07 16:24:14 +0300

    Addition of file + array

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    title: "Ein Gedi, Israel",
+    link: "https://unsplash.com/photos/boulder-beside-on-valley-and-bushes-VkZuNpXxdIg",
+  },
+  {
+    title: "Niagara Falls, NY",
+    link: "https://unsplash.com/photos/people-standing-on-cliff-near-waterfalls-during-daytime-61R8AY9YK0Q",
+  },
+  {
+    title: "Jim Thorpe, PA",
+    link: "https://unsplash.com/photos/green-and-brown-trees-during-daytime-MI5Xiovb-JI",
+  },
+  {
+    title: "Versailles, France",
+    link: "https://unsplash.com/photos/green-grass-field-with-trees-3Om3gtbgmiM",
+  },
+  {
+    title: "Everglades, FL",
+    link: "https://unsplash.com/photos/a-wooden-walkway-in-the-middle-of-a-forest-d1ekw-GUvn4",
+  },
+  {
+    title: "Golan Heights, Israel",
+    link: "https://unsplash.com/photos/yellow-petaled-flower-field-at-daytime-g_Abgn1slAM",
+  },
+];
+
+console.log(initialCards);
