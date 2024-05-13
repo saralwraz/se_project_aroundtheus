edb0e5e2c4851fb80dbe19429fdcb15feddce5e7
Sara Raz <saralwraz@gmail.com>
2024-05-13 22:33:07 +0300

    Modal update

--- scripts/index.js
+++ scripts/index.js
@@ -30,3 +30,17 @@ console.log(initialCards);
 function cardContent() {}
 
 cardContent();
+
+const profileEditButn = document.querySelector("#profile__edit-button");
+const profileEditmodal = document.querySelector("#profile__edit-modal");
+const profileCloseBtn = document.querySelector("#modal__close-button");
+
+function closePopup() {
+  profileEditmodal.classList.remove("modal__opened");
+}
+
+profileEditButn.addEventListener("click", () => {
+  profileEditmodal.classList.add("modal__opened");
+});
+
+profileCloseBtn.addEventListener("click", closePopup);
